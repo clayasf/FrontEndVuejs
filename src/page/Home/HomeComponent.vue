e9f355d5eb60046e74a8a9901aa256a2df3c601f
Clayton Freitas <freitas250@hotmail.com>
2024-07-25 10:37:11 -0300

    melhoria de descrição na tela inicial
    ajuste de descrição na tela inicial

--- src/page/Home/HomeComponent.vue
+++ src/page/Home/HomeComponent.vue
@@ -11,7 +11,7 @@
             <dt class="col-sm-3">Descrição:</dt>
             <dd class="col-sm-9">
               <p>Menus acima estão são SPA e não são recarregados ao click</p>
-              <p>Na pagina de CEP é possivel fazer a pesquisa no qual ao encontrar o cep é apresentado os dados na tela.</p>
+              <p>É possivel Clicar na imagem e ser redirecionado para o site da TradeUP.</p>
             </dd>
             <dt class="col-sm-3">
               <a href="https://www.tradeupgroup.com/" target="_blank">
@@ -19,12 +19,12 @@
               </a>
             </dt>
             <dd class="col-sm-9">
-              <p>É possivel Clicar na imagem e ser redirecionado para o site da TradeUP.</p>
+              <p>Na pagina de CEP é possivel fazer a pesquisa no qual ao encontrar o cep é apresentado os dados na tela.</p>
             </dd>
             <dt class="col-sm-3"></dt>
             <dd class="col-sm-9">
-              <p>Nossas soluções transformaram o modelo de venda de serviços no varejo brasileiro.</p>
-              
+              <p>Pagina de Cep trabalha com BackEnd em Laravel/Lumen.</p>
+              <p>Toda requisição volta um cep digitado no formulario da tela e esta guardando o log em uma tabela que pode ser acessada no endpoint do BackEnd "/historico"</p>              
             </dd>
           </dl>
         </div>
